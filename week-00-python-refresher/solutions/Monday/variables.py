dd59cdf14557affc4a9ee9c8be18fb83ca44ddc9
Ndahayo kevin <ndahayokevin1@gmail.com>
2026-05-03 13:55:53 -0500

    Git Basics: variables

---
+++ week-00-python-refresher/solutions/Monday/variables.py
@@ -0,0 +1,13 @@
+msg = "Hello, World!"
+number = 42
+pi = 3.14159
+is_valid = True
+names = ["Alice", "Bob", "Charlie"]
+person = {"name": "Alice", "age": 30, "city": "New York"}
+
+print (msg)
+print (number)
+print (pi)
+print (is_valid)
+print (names)
+print (person)
