5b1e1a2d44d62be35bf1819c26696cf90c67c989
kevinNdahayo <ndahayokevin1@gmail.com>
2026-05-30 12:07:03 -0700

    Finished working on strings

---
+++ week-01-big-o-arrays/solutions/strings.py
@@ -0,0 +1,21 @@
+a = "Hello"
+print (a)
+
+print("==================================")
+print("Strings are Arrays")
+print(a[1])
+print(a[-1])
+print(a[0])
+
+print("==================================")
+print("Looping through a string ")
+for i in range(len(a)):
+    print(i, a[i])
+
+
+print("==================================")
+print("Checking a text in a string")
+
+text = "The best things in life are free!"
+print("free" in text)
+
